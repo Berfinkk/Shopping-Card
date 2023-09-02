4e7584640c60d50ae21c03999199e296b5b5c4a5
Berfinkk <berfoqueen@gmail.com>
2023-09-02 11:31:36 +0300

    first commit

---
+++ my-app/src/pages/shop/shop.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Products } from "../../products";
+import { Product } from "./product";
+import "./shop.css";
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>Shopping App</h1>
+      </div>
+
+      <div className="products">
+        {Products.map((product) => (
+          <Product data={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ my-app/src/products.js
@@ -0,0 +1,60 @@
+import product1 from './assets/iphone.jpg'
+import product2 from './assets/samsung.jpg'
+import product3 from './assets/realme.jpg'
+import product4 from './assets/asus.jpg'
+import product5 from './assets/acer.jpg'
+import product6 from './assets/macbook.jpg'
+import product7 from './assets/beats.jpg'
+import product8 from './assets/sennheıser.jpg'
+
+export const Products=[
+    {
+        id:1,
+        productName:"Iphone",
+        price:50000,
+        productImage:product1
+    },
+    {
+        id:2,
+        productName:"Samsung",
+        price:15000,
+        productImage:product2
+    },
+    {
+        id:3,
+        productName:"Realme",
+        price:13500,
+        productImage:product3
+    },
+    {
+        id:4,
+        productName:"Asus",
+        price:20000,
+        productImage:product4
+    },
+    {
+        id:5,
+        productName:"Acer",
+        price:18000,
+        productImage:product5
+    },
+    {
+        id:6,
+        productName:"Macbook",
+        price:35000,
+        productImage:product6
+    },
+    {
+        id:7,
+        productName:"Beats Headphone",
+        price:35000,
+        productImage:product7
+   },
+   {
+       id:8,
+       productName:"Sennheıser Headphone",
+       price:35000,
+       productImage:product8
+   }
+
+]
